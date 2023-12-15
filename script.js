a9a054c42210ec3ab4a78ef263a109f93276578a
Cubemaster1999 <jannik.pflieger@gmail.com>
2023-12-15 12:13:41 +0100

    updated

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.getElementById('personal-projects-btn').addEventListener('click', function() {
+    document.getElementById('personal-projects').style.display = 'block';
+    document.getElementById('work-projects').style.display = 'none';
+    document.getElementById('research').style.display = 'none';
+    document.getElementById('interests').style.display = 'none';
+});
+
+document.getElementById('work-projects-btn').addEventListener('click', function() {
+    document.getElementById('work-projects').style.display = 'block';
+    document.getElementById('personal-projects').style.display = 'none';
+    document.getElementById('research').style.display = 'none';
+    document.getElementById('interests').style.display = 'none';
+});
+
 document.getElementById('interests-btn').addEventListener('click', function() {
     document.getElementById('interests').style.display = 'block';
     document.getElementById('research').style.display = 'none';
