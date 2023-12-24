d73037bff01a55321936e235cc811e20170c44b9
Cubemaster1999 <jannik.pflieger@gmail.com>
2023-12-24 18:01:55 +0100

    visual refactor

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+document.getElementById('about-btn').addEventListener('click', function() {
+    document.getElementById('about').style.display = 'block';
+    document.getElementById('personal-projects').style.display = 'none';
+    document.getElementById('work-projects').style.display = 'none';
+    document.getElementById('research').style.display = 'none';
+    document.getElementById('interests').style.display = 'none';
+});
+
 document.getElementById('personal-projects-btn').addEventListener('click', function() {
     document.getElementById('personal-projects').style.display = 'block';
+    document.getElementById('about').style.display = 'none';
     document.getElementById('work-projects').style.display = 'none';
     document.getElementById('research').style.display = 'none';
     document.getElementById('interests').style.display = 'none';
@@ -7,6 +16,7 @@ document.getElementById('personal-projects-btn').addEventListener('click', funct
 
 document.getElementById('work-projects-btn').addEventListener('click', function() {
     document.getElementById('work-projects').style.display = 'block';
+    document.getElementById('about').style.display = 'none';
     document.getElementById('personal-projects').style.display = 'none';
     document.getElementById('research').style.display = 'none';
     document.getElementById('interests').style.display = 'none';
@@ -14,6 +24,7 @@ document.getElementById('work-projects-btn').addEventListener('click', function(
 
 document.getElementById('interests-btn').addEventListener('click', function() {
     document.getElementById('interests').style.display = 'block';
+    document.getElementById('about').style.display = 'none';
     document.getElementById('research').style.display = 'none';
     document.getElementById('personal-projects').style.display = 'none';
     document.getElementById('work-projects').style.display = 'none';
@@ -21,6 +32,7 @@ document.getElementById('interests-btn').addEventListener('click', function() {
 
 document.getElementById('research-btn').addEventListener('click', function() {
     document.getElementById('research').style.display = 'block';
+    document.getElementById('about').style.display = 'none';
     document.getElementById('interests').style.display = 'none';
     document.getElementById('personal-projects').style.display = 'none';
     document.getElementById('work-projects').style.display = 'none';
